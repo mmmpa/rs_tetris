4c0318dddcea9293fa8fd96b5ceb14b3cce4bef3
mmmpa <mmmpa.mmmpa@gmail.com>
2020-08-19 06:24:11 +0900

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -50,9 +50,14 @@ pub enum Length {
 
 type NextMino = (Mino, Offset);
 
+pub trait MinosInner {}
+
 pub trait Minos {
-    fn right(self) -> (Mino, Offset);
-    fn left(self) -> (Mino, Offset);
+    type NextRight: MinosInner;
+    type NextLeft: MinosInner;
+
+    fn right(self) -> (Self::NextRight, Offset);
+    fn left(self) -> (Self::NextLeft, Offset);
     fn cells(&self) -> &[&[u8]];
 }
 
@@ -60,9 +65,16 @@ pub trait Minos {
 macro_rules! mino_variant {
     ( $($element:tt),* $(,)? ) => {
         $(pub struct $element;)*
+        $(impl MinosInner for $element {})*
 
         pub enum Mino {
             $($element($element),)*
+        }
+
+        impl From<$elemet> for Mino {
+            fn from(inner: self) -> Self {
+                Self::$element(inner)
+            }
         }
 
         impl Mino {
@@ -93,12 +105,15 @@ mino_variant!(A0, A1, A2, A3, B0, C0, C1, C2, C3, D0, D1, D2, D3, E0, E1, E2, E3
 macro_rules! mino {
     ( $now:tt, $right:tt, $right_x:tt, $right_y:tt, $left:tt, $left_x:tt, $left_y:tt, $cells:expr ) => {
         impl Minos for $now {
-            fn right(self) -> (Mino, Offset) {
-                next_mino(Mino::$right($right), Length::$right_x, Length::$right_y)
+            type NextRight = $right;
+            type NextLeft = $left;
+
+            fn right(self) -> (Self::NextRight, Offset) {
+                next_mino($right, Length::$right_x, Length::$right_y)
             }
 
-            fn left(self) -> (Mino, Offset) {
-                next_mino(Mino::$left($left), Length::$left_x, Length::$left_y)
+            fn left(self) -> (Self::NextLeft, Offset) {
+                next_mino($left, Length::$left_x, Length::$left_y)
             }
 
             fn cells(&self) -> &[&[u8]] {
@@ -108,7 +123,7 @@ macro_rules! mino {
     };
 }
 
-fn next_mino(next: Mino, x: Length, y: Length) -> NextMino {
+fn next_mino<T>(next: T, x: Length, y: Length) -> (T, Offset) {
     (next, Offset { x, y })
 }
 
@@ -119,112 +134,113 @@ fn test(v: Mino) {
     let (next, _) = next.left();
 }
 
-// *o**
+// ⬛⬜⬛⬛
 // -----
-// *
-// o
-// *
-// *
+// ⬛
+// ⬜
+// ⬛
+// ⬛
 // -----
-// **o*
+// ⬛⬛⬜⬛
 // -----
-// *
-// *
-// o
-// *
+// ⬛
+// ⬛
+// ⬜
+// ⬛
 mino!(A0, A1, P1, M1, A3, P1, M2, &[&[1, 1, 1, 1]]);
 mino!(A1, A2, M2, P1, A0, M1, P1, &[&[1], &[1], &[1], &[1]]);
 mino!(A2, A3, P2, M2, A1, P2, M1, &[&[1, 1, 1, 1]]);
 mino!(A3, A0, M1, P2, A2, M2, P2, &[&[1], &[1], &[1], &[1]]);
 
-// **
-// **
+// ⬛⬛
+// ⬛⬛
 mino!(B0, B0, No, No, B0, No, No, &[&[1, 1], &[1, 1]]);
 
-//  **
-// *o
+// 　⬛⬛
+// ⬛⬜
 // -----
-// *
-// o*
-//  *
+// ⬛
+// ⬜⬛
+// 　⬛
 // -----
-//  o*
-// **
+// 　⬜⬛
+// ⬛⬛
 // -----
-// *
-// *o
-//  *
+// ⬛
+// ⬛⬜
+// 　⬛
 mino!(C0, C1, P1, No, C3, No, No, &[&[0, 1, 1], &[1, 1]]);
 mino!(C1, C2, M1, P1, C0, M1, No, &[&[1], &[1, 1], &[0, 1]]);
 mino!(C2, C3, No, M1, C1, P1, M1, &[&[0, 1, 1], &[1, 1]]);
 mino!(C3, C0, No, No, C2, No, P1, &[&[1], &[1, 1], &[0, 1]]);
 
-// **
-//  o*
+// ⬛⬛
+// 　⬜⬛
 // -----
-//  *
-// o*
-// *
+//  ⬛
+// ⬜⬛
+// 　⬛
 // -----
-// *o
-//  **
+// ⬛⬜
+// 　⬛⬛
 // -----
-//  *
-// *o
-// *
+// 　⬛
+// ⬛⬜
+// ⬛
 mino!(D0, D1, P1, No, D3, No, No, &[&[1, 1], &[0, 1, 1]]);
 mino!(D1, D2, M1, P1, D0, M1, No, &[&[0, 1], &[1, 1], &[1]]);
 mino!(D2, D3, No, M1, D1, P1, M1, &[&[1, 1], &[0, 1, 1]]);
 mino!(D3, D0, No, No, D2, No, P1, &[&[0, 1], &[1, 1], &[1]]);
 
-// *
-// *o*
+// ⬛
+// ⬛⬜⬛
 // -----
-// **
-// o
-// *
+// ⬛⬛
+// ⬜
+// ⬛
 // -----
-// *o*
-//   *
+// ⬛⬜⬛
+// 　　⬛
 // -----
-//  *
-//  o
-// **
+// 　⬛
+// 　⬜
+// ⬛⬛
 mino!(E0, E1, P1, No, E3, No, No, &[&[1], &[1, 1, 1]]);
 mino!(E1, E2, M1, P1, E0, M1, No, &[&[1, 1], &[1], &[1]]);
 mino!(E2, E3, No, M1, E1, P1, M1, &[&[1, 1, 1], &[0, 0, 1]]);
 mino!(E3, E0, No, No, E2, No, P1, &[&[0, 1], &[0, 1], &[1, 1]]);
 
-//   *
-// *o*
+// 　　⬛
+// ⬛⬜⬛
 // -----
-// *
-// o
-// **
+// ⬛
+// ⬜
+// ⬛⬛
 // -----
-// *o*
-// *
+// ⬛⬜⬛
+// ⬛
 // -----
-// **
-//  o
-//  *
+// ⬛⬛
+// 　⬜
+// 　⬛
 mino!(F0, F1, P1, No, F3, No, No, &[&[0, 0, 1], &[1, 1, 1]]);
 mino!(F1, F2, M1, P1, F0, M1, No, &[&[1], &[1], &[1, 1]]);
 mino!(F2, F3, No, M1, F1, P1, M1, &[&[1, 1, 1], &[1]]);
 mino!(F3, F0, No, No, F2, No, P1, &[&[1, 1], &[0, 1], &[0, 1]]);
 
-//  *
-// *o*
+// 　⬛
+// ⬛⬜⬛
 // -----
-// *
-// o*
-// *
+// ⬛
+// ⬜⬛
+// ⬛
 // -----
-// *o*
-//  *
-//  *
-// *o
-//  *
+// ⬛⬜⬛
+// 　⬛
+// -----
+// 　⬛
+// ⬛⬜
+// 　⬛
 mino!(G0, G1, P1, No, G3, No, No, &[&[0, 1], &[1, 1, 1]]);
 mino!(G1, G2, M1, P1, G0, M1, No, &[&[1], &[1, 1], &[1]]);
 mino!(G2, G3, No, M1, G1, P1, M1, &[&[1, 1, 1], &[0, 1]]);

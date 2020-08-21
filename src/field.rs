9a865dc7c522ecf041b8c17d26a2903055de58c5
mmmpa <mmmpa.mmmpa@gmail.com>
2020-08-22 04:48:49 +0900

    updated

---
+++ src/field.rs
@@ -0,0 +1,187 @@
+use crate::*;
+use core::mem;
+
+#[derive(Debug)]
+pub struct Field {
+    counts: [usize; FIELD_H],
+    rows: [[bool; FIELD_W]; FIELD_H],
+}
+
+impl Field {
+    pub fn new() -> Self {
+        let mut rows = [0; FIELD_H];
+        for i in 0..FIELD_H {
+            rows[i] = i;
+        }
+
+        Self {
+            counts: [0; FIELD_H],
+            rows: [[false; FIELD_W]; FIELD_H],
+        }
+    }
+
+    pub fn test(&self, x: usize, y: usize) -> bool {
+        self.rows[y][x]
+    }
+
+    pub fn set(&mut self, x: usize, y: usize) {
+        self.rows[y][x] = true;
+        self.counts[y] += 1;
+    }
+
+    pub fn delete(&mut self, y: usize) -> bool {
+        if self.counts[y] != FIELD_W {
+            return false;
+        }
+
+        self.counts[y] = 0;
+        self.rows[y] = [false; FIELD_W];
+
+        true
+    }
+
+    // MUST float from above after delete multiline
+    pub fn float(&mut self, y: usize) {
+        let mut now = y;
+        while now > 0 {
+            let up_row = now - 1;
+            if self.counts[up_row] == 0 {
+                return;
+            }
+
+            self.rows.swap(now, up_row);
+            self.counts.swap(now, up_row);
+
+            now -= 1;
+        }
+    }
+}
+
+#[cfg(test)]
+mod filed_tester {
+    use crate::*;
+    use std::prelude::v1::*;
+
+    pub fn print_field(f: &Field, h: usize) -> String {
+        let mut canvas = vec![vec!["⬜"; FIELD_W]; h];
+
+        let top = FIELD_H - h;
+        for y in 0..h {
+            for x in 0..FIELD_W {
+                if f.test(x, top + y) {
+                    canvas[y as usize][x as usize] = "⬛";
+                }
+            }
+        }
+
+        canvas
+            .into_iter()
+            .flat_map(|mut s| {
+                s.push("\n");
+                s
+            })
+            .collect::<Vec<_>>()
+            .join("")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::field::filed_tester::print_field;
+    use crate::*;
+    use std::prelude::v1::*;
+
+    #[test]
+    fn test_float() {
+        let mut f = Field::new();
+
+        f.set(0, 19);
+        f.set(3, 18);
+        f.set(4, 18);
+        f.set(2, 17);
+
+        let s = print_field(&f, 4);
+        assert_eq!([0, 1, 2, 1], f.counts[16..20]);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬛⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬛⬜⬜⬜⬜⬜\n\
+                ⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+            ",
+            s
+        );
+
+        f.float(18);
+
+        let s = print_field(&f, 4);
+        assert_eq!([0, 2, 1, 1], f.counts[16..20]);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬛⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬛⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+            ",
+            s
+        );
+
+        f.float(19);
+
+        let s = print_field(&f, 4);
+        assert_eq!([0, 1, 2, 1], f.counts[16..20]);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬛⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬛⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬛⬜⬜⬜⬜⬜⬜⬜\n\
+            ",
+            s
+        );
+    }
+
+    #[test]
+    fn test_delete() {
+        let mut f = Field::new();
+
+        for x in 0..FIELD_W {
+            f.set(x, 19);
+        }
+        f.set(3, 18);
+
+        let s = print_field(&f, 3);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬜⬜⬜⬜⬜⬜\n\
+                ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛\n\
+            ",
+            s
+        );
+
+        assert_eq!(false, f.delete(18));
+        let s = print_field(&f, 3);
+        assert_eq!(1, f.counts[18]);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬜⬜⬜⬜⬜⬜\n\
+                ⬛⬛⬛⬛⬛⬛⬛⬛⬛⬛\n\
+            ",
+            s
+        );
+
+        assert_eq!(true, f.delete(19));
+        let s = print_field(&f, 3);
+        assert_eq!(0, f.counts[19]);
+        assert_eq!(
+            "\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬛⬜⬜⬜⬜⬜⬜\n\
+                ⬜⬜⬜⬜⬜⬜⬜⬜⬜⬜\n\
+            ",
+            s
+        );
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -5,65 +5,11 @@
 #[macro_use]
 extern crate std;
 
+mod field;
 mod mino;
 
-use core::mem::replace;
+pub use field::*;
 pub use mino::*;
 
 pub const FIELD_W: usize = 10;
 pub const FIELD_H: usize = 20;
-
-pub struct Field {
-    row_pointers: [usize; FIELD_H],
-    row_counts: [usize; FIELD_H],
-    actual_rows: [[bool; FIELD_W]; FIELD_H],
-}
-
-impl Field {
-    pub fn new() -> Self {
-        let mut rows = [0; FIELD_H];
-        for i in 0..FIELD_H {
-            rows[i] = i;
-        }
-
-        Self {
-            row_pointers: rows,
-            row_counts: [0; FIELD_H],
-            actual_rows: [[false; FIELD_W]; FIELD_H],
-        }
-    }
-
-    pub fn pointer_y(&self, y: usize) -> usize {
-        self.row_pointers[y]
-    }
-
-    pub fn set(&mut self, x: usize, pointer_y: usize) {
-        self.actual_rows[pointer_y][x] = true;
-        self.row_counts[pointer_y] += 1;
-    }
-
-    pub fn delete(&mut self, pointer_y: usize) -> bool {
-        if self.row_counts[pointer_y] != FIELD_W {
-            return false;
-        }
-
-        self.row_counts[pointer_y] = 0;
-        self.actual_rows[pointer_y] = [false; FIELD_W];
-
-        true
-    }
-
-    pub fn shift(&mut self, y: usize) {
-        let mut now = y;
-        loop {
-            let above_pointer = self.row_pointers[now - 1];
-            if self.row_counts[above_pointer] != 0 {
-                return;
-            }
-            let now_pointer = self.row_pointers[now];
-            self.row_pointers[now] = above_pointer;
-            self.row_pointers[now - 1] = now_pointer;
-            now -= 1;
-        }
-    }
-}
